74ed177f2fa39d872e3b7f0869893c2efdbfc30f
Alexander Lent <git@xanderlent.com>
2015-10-18 06:04:02 -0700

    Backend is now a single class, no interface

--- app/src/main/java/com/xanderlent/android/mmatc/Backend.java
+++ app/src/main/java/com/xanderlent/android/mmatc/Backend.java
@@ -1,11 +1,49 @@
 package com.xanderlent.android.mmatc;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
-public interface Backend {
-    Collection<Plane> getPlanes();
-    void turnPlane(Plane plane, Direction newDirection);
-    void changeAltitude(Plane plane, int newAltitude);
-    void createPlane(Position where);
-    void tick();
+public class Backend {
+    private Random random;
+    private Collection<Plane> planes;
+
+    public Backend() {
+        random = new Random();
+        planes = Collections.synchronizedCollection(new ArrayList<Plane>());
+    }
+
+    public synchronized Collection<Plane> getPlanes() {
+        return Collections.unmodifiableCollection(planes);
+    }
+
+    public synchronized void turnPlane(Plane plane, Direction newDirection) {
+        if(!planes.contains(plane)) {
+            return;  // TODO: exception?
+        }
+        plane.setDirection(newDirection);
+    }
+
+    public synchronized void changeAltitude(Plane plane, int newAltitude) {
+        if(!planes.contains(plane)) {
+            return;  // TODO: exception?
+        }
+        plane.setTargetAltitude(newAltitude);
+    }
+
+    public synchronized void createPlane(Position where) {
+        Direction direction = Direction.NORTH_EAST;
+        int destIndex = random.nextInt(6);
+        Plane plane = new Plane("Concorde", where, direction, destIndex);
+        planes.add(plane);
+    }
+
+    public synchronized void tick() {
+        for(Plane plane : planes) {
+            plane.setPosition(plane.getPosition().towardsDirection(plane.getDirection()));
+            plane.setAltitudeImmediately(plane.getAltitude() +
+                    Integer.signum(plane.getTargetAltitude() - plane.getAltitude()), false);
+        }
+    }
 }
