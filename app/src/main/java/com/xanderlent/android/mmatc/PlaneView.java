06c3920adc4ec2f2acfaf42b500e200581b45ae3
Alex Kirchhoff <dubhacks-mmatc@usertracker.org>
2015-10-18 12:09:35 -0700

    Make PlaneView show more data.

--- app/src/main/java/com/xanderlent/android/mmatc/PlaneView.java
+++ app/src/main/java/com/xanderlent/android/mmatc/PlaneView.java
@@ -5,7 +5,9 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,6 +23,9 @@ public class PlaneView extends View {
         void onSelectionChanged(PlaneView planeView, Plane selectedPlane);
     }
 
+    private Paint fillPaint;
+    private Paint strokePaint;
+    private TextPaint textPaint;
     private Collection<Plane> planes;
     private Plane selectedPlane;
     private SelectionChangeCallback selectionChangeCallback;
@@ -47,6 +52,15 @@ public class PlaneView extends View {
 
         a.recycle();
 
+        fillPaint = new Paint();
+        fillPaint.setStyle(Paint.Style.FILL);
+        strokePaint = new Paint();
+        strokePaint.setStyle(Paint.Style.STROKE);
+        textPaint = new TextPaint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
         planes = Collections.emptyList();
         selectedPlane = null;
         selectionChangeCallback = null;
@@ -65,40 +79,75 @@ public class PlaneView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.WHITE);
-        canvas.drawRect(new Rect(0, 0, getWidth(), getHeight()), paint);
-
-        Paint strokePaint = new Paint();
-        strokePaint.setStyle(Paint.Style.STROKE);
         strokePaint.setStrokeWidth(2);
         strokePaint.setColor(Color.BLACK);
-        Paint fillPaint = new Paint();
-        fillPaint.setStyle(Paint.Style.FILL);
+        fillPaint.setColor(Color.DKGRAY);
+
+        canvas.drawRect(new Rect(0, 0, getWidth(), getHeight()), fillPaint);
+
+        canvas.save();
+        float minimalDimension = Math.min(getWidth(), getHeight());
+        canvas.translate((getWidth() - minimalDimension) / 2, (getHeight() - minimalDimension) / 2);
+        fillPaint.setColor(Color.WHITE);
+        canvas.drawRect(0, 0, minimalDimension, minimalDimension, fillPaint);
+        canvas.drawRect(0, 0, minimalDimension, minimalDimension, strokePaint);
+
+        textPaint.setTextSize(minimalDimension / GRID_HEIGHT);
+
+        Path path = new Path();
+        {
+            float bigness = minimalDimension / GRID_WIDTH / 2;
+            path.moveTo(0, -bigness);
+            path.lineTo(bigness, bigness);
+            path.lineTo(0, bigness / 2);
+            path.lineTo(-bigness, bigness);
+            path.close();
+        }
+
         for(Plane plane : planes) {
             Position position = plane.getPosition();
-            float left = (float)position.getX() * getWidth() / GRID_WIDTH;
-            float top = (float)position.getY() * getHeight() / GRID_HEIGHT;
-            float right = left + (float)getWidth() / GRID_WIDTH;
-            float bottom = top + (float)getHeight() / GRID_HEIGHT;
+            float left = position.getX() * minimalDimension / GRID_WIDTH;
+            float top = position.getY() * minimalDimension / GRID_HEIGHT;
+            float right = left + minimalDimension / GRID_WIDTH;
+            float bottom = top + minimalDimension / GRID_HEIGHT;
             if(plane == selectedPlane) {
                 fillPaint.setColor(Color.GREEN);
             }else{
                 fillPaint.setColor(Color.YELLOW);
             }
-            canvas.drawRect(left, top, right, bottom, fillPaint);
-            canvas.drawRect(left, top, right, bottom, strokePaint);
+            canvas.save();
+            canvas.translate((left + right) / 2, (top + bottom) / 2);
+            switch(plane.getDirection()) {
+                case NORTH: canvas.rotate(0); break;
+                case NORTH_EAST: canvas.rotate(45); break;
+                case EAST: canvas.rotate(90); break;
+                case SOUTH_EAST: canvas.rotate(135); break;
+                case SOUTH: canvas.rotate(180); break;
+                case SOUTH_WEST: canvas.rotate(225); break;
+                case WEST: canvas.rotate(270); break;
+                case NORTH_WEST: canvas.rotate(335); break;
+            }
+            canvas.drawPath(path, fillPaint);
+            canvas.drawPath(path, strokePaint);
+            //canvas.drawRect(left, top, right, bottom, fillPaint);
+            //canvas.drawRect(left, top, right, bottom, strokePaint);
+            canvas.restore();
+            canvas.drawText(plane.getName() + plane.getAltitude(), (left + right) / 2, (top + bottom * 3) / 4, textPaint);
         }
+
+        canvas.restore();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            float minimalDimension = Math.min(getWidth(), getHeight());
             float touchX = event.getX();
             float touchY = event.getY();
-            int gridX = (int)(touchX / getWidth() * GRID_WIDTH);
-            int gridY = (int)(touchY / getHeight() * GRID_HEIGHT);
+            touchX -= (getWidth() - minimalDimension) / 2;
+            touchY -= (getHeight() - minimalDimension) / 2;
+            int gridX = (int)(touchX / minimalDimension * GRID_WIDTH);
+            int gridY = (int)(touchY / minimalDimension * GRID_HEIGHT);
             Position position = new Position(gridX, gridY);
             Plane selectedPlane = null;
             for(Plane plane : planes) {
