22506a92ad77968a8ee88d7e6958a86be95276f6
ScarletCoder <tedle1997@yahoo.com>
2015-10-17 23:23:05 -0700

    Commented out the box and use the fab as the launching button

--- app/src/main/java/com/xanderlent/android/mmatc/MainActivity.java
+++ app/src/main/java/com/xanderlent/android/mmatc/MainActivity.java
@@ -1,12 +1,13 @@
 package com.xanderlent.android.mmatc;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,22 +18,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
-        Button button_start = (Button) findViewById(R.id.startButton);
-        button_start.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //TODO make intent and connect to the gaming activity
+            public void onClick(View view) {
+                Snackbar.make(view, "This will launch the game", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+
+//        Button button_start = (Button) findViewById(R.id.startButton);
+//        button_start.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //TODO make intent and connect to the gaming activity
+//            }
+//        });
     }
 
     @Override
